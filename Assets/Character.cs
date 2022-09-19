729c521026b862bf2f0f9d5cdffef21fe5c611d2
Dmitry_Starovoytov <dmitr.starovoitov@kaseya.com>
2022-09-20 00:42:46 +0300

    add scene

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -5,6 +5,7 @@ public class Character : MonoBehaviour
     public float gravity = -9.81f;
     public float speed = 10.0f;
     public float jumpSpeed = 20.0f;
+    public float rotationSpeed = 5.0f;
 
     private float ySpeed;
 
@@ -24,13 +25,9 @@ public class Character : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
-
-        Debug.Log("Sped jump" + jumpSpeed);
-        Debug.Log("IsGrounded:" + Controller.isGrounded);
-
         ySpeed += gravity * Time.deltaTime;
 
-        if (controller.isGrounded) 
+        if (Controller.isGrounded) 
         {
             ySpeed = -0.5f;
             if (Input.GetButtonDown("Jump"))
@@ -38,11 +35,17 @@ public class Character : MonoBehaviour
                 ySpeed = jumpSpeed;
             }
         }
+        else 
+        {
+            vertical = 0.0f;
+            horizontal = 0.0f;
+        }
 
         Vector3 movement = new Vector3(horizontal * speed, ySpeed, vertical * speed);
+        print(movement);
 
         Controller.Move(transform.TransformDirection(movement)  * Time.deltaTime);
-        Controller.transform.Rotate(Vector3.up, rotation);
+        Controller.transform.Rotate(Vector3.up, rotation * rotationSpeed);
     }
 
 
