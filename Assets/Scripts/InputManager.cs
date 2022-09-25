a6f3d39d7e1d1be9cbc48f019acd4519481806d9
Dmitry_Starovoytov <dmitr.starovoitov@kaseya.com>
2022-09-25 03:08:15 +0300

    Set up annimations

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Vector3 movementInput { get; private set; }
+   
+    void Update()
+    {
+        //movementInput = new Vector3(horizontal, 0.0f, vertical);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,9 +7,6 @@ public class PlayerController : MonoBehaviour
     public float sprintSpeed = 5.0f;
     public float rotationSpeed = 100f;
     public float jumpSpeed = 7.0f;
-
-    public AnimationClip spawnAnimationClip;
-    public AnimatorOverrideController[] overrideControllers;
 
     private float animationBlendSpeed = 0.2f;
     private float targetAnimationSpeed = 0.0f;
@@ -26,96 +21,59 @@ public class PlayerController : MonoBehaviour
     public Animator Animator { get { return animator = animator ?? GetComponent<Animator>(); } }
 
 
-
     private bool isSprint = false;
 
     private float speedY = 0.0f;
     private float gravity = -9.81f;
     private bool isJumping = false;
-    private bool isSpawnTime = false; 
-    private bool isAlive = false; 
-    private bool isAttack= false; 
+
+    private bool isScriptAvailible = false;
+    private bool isReadyToDie = false;
 
 
     void Start()
     {
-        StartCoroutine(PlaySpawn());
-        isAlive = true;
-        //animator.fireEvents = false;
-    }
-
-    public void MeleeAttackStart(int num) 
-    {
-        Debug.Log(num);
-    }
-
-    public void MeleeAttackEnd(int num)
-    {
-        Debug.Log(num);
-    }
-
-    public void SetAnimations(AnimatorOverrideController overrideController) 
-    {
-        Animator.runtimeAnimatorController = overrideController;
-    }
-
-    private IEnumerator PlaySpawn() 
-    {
         Animator.SetTrigger("Spawn");
-        yield return new WaitForSeconds(spawnAnimationClip.length);
-        isSpawnTime = true;
     }
 
-    private IEnumerator PlayAttack()
+    public void SpawnFinish() 
     {
-        Animator.SetTrigger("Attack");
-        isAttack = true;
-        var clip = new List<AnimationClip>(Animator.runtimeAnimatorController.animationClips).Find(x => x.name.Contains("Combo"));
-        yield return new WaitForSeconds(clip.length);
-        SetAnimations(overrideControllers[Random.Range(0, 4)]);
-        isAttack = false;
-    }
-
-    private IEnumerator WaitUntilJumpIsFinishedAndDie()
-    {
-        yield return new WaitForSeconds(1.5f);
-        Animator.SetTrigger("Die");
-        isAlive = false;
-
+        isScriptAvailible = true;
     }
 
     void Update()
     {
-        if(isSpawnTime && isAlive && !isAttack) 
+        if (isScriptAvailible) 
         {
             float vertical = Input.GetAxis("Vertical");
             float horizontal = Input.GetAxis("Horizontal");
 
-
-            if (Input.GetMouseButtonDown(0) && !isJumping)
+            //Attack
+            if (Input.GetMouseButtonDown(0))
             {
-                StartCoroutine(PlayAttack());
+                Animator.SetInteger("RandomAttack", Random.Range(1, 5));
+                Animator.SetTrigger("Attack");
             }
 
+            //Die
             if (Input.GetKeyDown(KeyCode.E))
-            { 
-                if (isJumping) 
-                {
-                    StartCoroutine(WaitUntilJumpIsFinishedAndDie());
-                }
-                else{
-                    Animator.SetTrigger("Die");
-                    isAlive = false;
-                }
+            {
+                isReadyToDie = true;
+            }
+            if (isReadyToDie && speedY == 0.0f)
+            {
+                Animator.SetTrigger("Die");
+                isScriptAvailible = false;
             }
 
+            //Jump
             if (Input.GetButtonDown("Jump") && !isJumping)
             {
                 isJumping = true;
                 Animator.SetTrigger("Jump");
                 speedY += jumpSpeed;
             }
-            if (!Controller.isGrounded)
+            if (!Controller.isGrounded && isJumping)
             {
                 speedY += gravity * Time.deltaTime;
             }
@@ -125,7 +83,6 @@ public class PlayerController : MonoBehaviour
             }
 
             Animator.SetFloat("SpeedY", speedY / jumpSpeed);
-
             if (isJumping && speedY < 0.0f)
             {
                 if (Physics.Raycast(transform.position, Vector3.down, 1.0f, LayerMask.GetMask("Default")))
@@ -135,8 +92,9 @@ public class PlayerController : MonoBehaviour
                 }
             }
 
-            isSprint = Input.GetKey(KeyCode.LeftShift);
 
+            //Move
+            isSprint = Input.GetKey(KeyCode.LeftShift);
             Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
 
             Vector3 velocity = Quaternion.Euler(0.0f, CharacterCamera.transform.rotation.eulerAngles.y, 0.0f) * movement.normalized;
