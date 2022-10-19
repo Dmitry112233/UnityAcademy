fee57277b394bbe569379d4d9de575bfbd06411e
Dmitry_Starovoytov <dmitr.starovoitov@kaseya.com>
2022-10-19 22:21:23 +0300

    init

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = GenerateVertices();
+        mesh.triangles = GenerateTriangles();
+    }
+
+    Vector3[] GenerateVertices() 
+    {
+        return new Vector3[]
+        {
+            new Vector3(0.0f, 0.5f, 1.0f),
+            new Vector3(1.0f, 0.5f, 0.0f),
+            new Vector3(-1.0f, 0.5f, 0.0f),
+            new Vector3(0.0f, 0.5f, -1.0f),
+            new Vector3(1.0f, -0.5f, 0.0f),
+            new Vector3(0.0f, -0.5f, -1.0f),
+            new Vector3(-1.0f, -0.5f, 0.0f),
+            new Vector3(0.0f, -0.5f, 1.0f),
+        };
+    }
+
+    int[] GenerateTriangles()
+    {
+        return new int[] { 0, 1, 3, 0, 3, 2, 3, 1, 4, 3, 4, 5, 3, 5, 6, 6, 2, 3, 5, 4, 7, 6, 5, 7, 0, 2, 7, 2, 6, 7, 0, 4, 1, 0, 7, 4};
+    }
+}
